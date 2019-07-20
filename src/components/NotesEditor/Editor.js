295b61288af578568f23433bfdc52a0d93b69a40
Conor <conor909@gmail.com>
2019-07-20 17:03:32 +0100

    editor styling

--- src/components/NotesEditor/Editor.js
+++ src/components/NotesEditor/Editor.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { Editor, EditorState, RichUtils } from 'draft-js'
+import BlockStyleControls from './BlockStyleControls'
+import InlineStyleControls from './InlineStyleControls'
 import 'draft-js/dist/Draft.css'
 import './editor.scss'
 
@@ -74,23 +76,27 @@ export default function (props) {
       <Form.Control
         type='text'
         value={noteTitle}
-        className='editor--title-input'
+        className='editor--edit-title'
         onChange={onChangeTitle} />
-      <BlockStyleControls
-        editorState={editorState}
-        onToggle={toggleBlockType} />
-      <InlineStyleControls
-        editorState={editorState}
-        onToggle={toggleInlineStyle} />
-      <Editor
-        handleKeyCommand={handleKeyCommand}
-        editorState={editorState}
-        onChange={handleChangeEditorState}
-        blockStyleFn={getBlockStyle}
-        customStyleMap={styleMap}
-        onTab={onTab}
-        placeholder='Tell a story...'
-        spellCheck />
+      <div className='editor--edit-styles'>
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={toggleBlockType} />
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={toggleInlineStyle} />
+      </div>
+      <div className='editor--edit-note'>
+        <Editor
+          handleKeyCommand={handleKeyCommand}
+          editorState={editorState}
+          onChange={handleChangeEditorState}
+          blockStyleFn={getBlockStyle}
+          customStyleMap={styleMap}
+          onTab={onTab}
+          placeholder='Tell a story...'
+          spellCheck />
+      </div>
       {
         note.note !== editorState &&
         <Button
@@ -110,85 +116,4 @@ const styleMap = {
     fontSize: 16,
     padding: 2
   }
-}
-
-function StyleButton (props) {
-  function onToggle (e) {
-    e.preventDefault()
-    props.onToggle(props.style)
-  }
-
-  let className = 'RichEditor-styleButton'
-  if (props.active) {
-    className += ' RichEditor-activeButton'
-  }
-
-  return (
-    <span className={className} onMouseDown={onToggle}>
-      {props.label}
-    </span>
-  )
-
-}
-
-var INLINE_STYLES = [
-  { label: 'Bold', style: 'BOLD' },
-  { label: 'Italic', style: 'ITALIC' },
-  { label: 'Underline', style: 'UNDERLINE' },
-  { label: 'Monospace', style: 'CODE' }
-];
-
-const InlineStyleControls = (props) => {
-  var currentStyle = props.editorState.getCurrentInlineStyle()
-  return (
-    <div className='RichEditor-controls'>
-      {
-        INLINE_STYLES.map(type =>
-          <StyleButton
-            key={type.label}
-            active={currentStyle.has(type.style)}
-            label={type.label}
-            onToggle={props.onToggle}
-            style={type.style}
-          />
-        )
-      }
-    </div>
-  )
-}
-
-const BLOCK_TYPES = [
-  { label: 'H1', style: 'header-one' },
-  { label: 'H2', style: 'header-two' },
-  { label: 'H3', style: 'header-three' },
-  { label: 'H4', style: 'header-four' },
-  { label: 'H5', style: 'header-five' },
-  { label: 'H6', style: 'header-six' },
-  { label: 'Blockquote', style: 'blockquote' },
-  { label: 'UL', style: 'unordered-list-item' },
-  { label: 'OL', style: 'ordered-list-item' },
-  { label: 'Code Block', style: 'code-block' },
-]
-
-const BlockStyleControls = (props) => {
-  const { editorState } = props
-  const selection = editorState.getSelection()
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType()
-
-  return (
-    <div className='RichEditor-controls'>
-      {BLOCK_TYPES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  )
 }
---
+++ src/components/NotesEditor/StyleButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function StyleButton(props) {
+
+  function onToggle (e) {
+    e.preventDefault()
+    props.onToggle(props.style)
+  }
+
+  let className = 'RichEditor-styleButton'
+  if (props.active) {
+    className += ' RichEditor-activeButton'
+  }
+
+  return (
+    <span className={className} onMouseDown={onToggle}>
+      {props.label}
+    </span>
+  )
+}
